01fbfe772557c5461c6e990b5cf42f7e1cef57ec
Xurshidabonu <xurshidabonushukurjonova17@gmail.com>
2024-02-02 21:49:19 +0500

    1.0 uyga vazifaning masalalari

---
+++ masala3.py
@@ -0,0 +1,5 @@
+N = int(input())
+if N % 2 == 0:
+    print('Second player')
+else:
+    print('First player')
---
+++ masala4.py
@@ -0,0 +1,3 @@
+a= [int(input(f"{i+1}-gnom uchun tangalar sonini kiriting: ")) for i in range(7)]
+S=int(input())
+print(S-(sum(a)))
